1ccb432be4f87b71742a3025e749f53c9eb152b8
Zane2019 <zhangzhiming2017@gmail.com>
2019-12-18 20:53:19 +0800

    Create kdtree.h

---
+++ Search/KDTree/kdtree.h
@@ -0,0 +1,33 @@
+#include<vector>
+#include<iostream>
+#include<memory>
+
+struct Data{
+    int x;
+    int y;
+    Data(int val1,int val2):x(val1),y(val2){}
+};
+
+
+template<typename T>
+struct treeNode{
+    T data;
+    int split;
+    std::shared_ptr<treeNode> left;
+    std::shared_ptr<treeNode> right;
+    treeNode(T value,int id):data(value),split(id),left(nullptr),right(nullptr){}
+};
+
+
+class kdtree
+{
+private:
+    std::shared_ptr<treeNode<Data>> root_;
+public:
+    kdtree(std::shared_ptr<treeNode<Data>> val):root_(val){}
+    kdtree():root_(nullptr){}
+    void BuildKDTree(std::vector<Data> val);
+    void SerachNearest(Data target, Data &nearestpoint, double & distance);
+    void ChooseSplit(std::vector<Data>val)
+    ~kdtree();
+};
